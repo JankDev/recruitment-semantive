1fd5de0a15cd059fafb264cebfbe56d1eaeb9a44
Robert Kraut <robert.s.kraut@gmail.com>
2020-06-24 18:28:28 +0200

    defined the project structure and added angular material

--- shopping-cart-frontend/src/app/app.module.ts
+++ shopping-cart-frontend/src/app/app.module.ts
@@ -1,18 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {CoreModule} from "./_core/core.module";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
